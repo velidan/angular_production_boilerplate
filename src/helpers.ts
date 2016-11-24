841469bd0e92c9154c599735d3ff62d156f2352c
Sviatoslav Barbutsa <sbarbut@softserveinc.com>
2016-11-24 12:26:47 +0200

    - added documentation for the sprite engine

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,7 +3,21 @@ import './assets/styles/main.scss';
 class Helpers {
 
 
+	/**
+	*
+	* It will enable the svg engine. After that you can use your svg in you html like:
+
+	  <svg class="svg-icon">
+	    <use xlink:href="#icon-rocket"></use> 
+	  </svg>
+
+	  Where #icon-rocket    is the id of your svg (icon-    is a system prefix. So the origin svg name is: rocket.svg)
+	*
+	* Svg sources placed it the src/assets/svg
+	*/
+
 	public initSvgEngine() {
+
 
 		var __svg__: any = { path: './assets/svg/**/*.svg', name: '[hash].spritemap.svg' };
 
