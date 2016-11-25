9caa2e8b170b53c558cb32f9b1de31e8175ed1d6
Sviatoslav Barbutsa <sbarbut@softserveinc.com>
2016-11-25 10:49:22 +0200

    - added testing (karma)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const NODE_ENV = process.env.NODE_ENV || 'development',
 	confType = NODE_ENV === 'production' ? 'prod' : 'dev';
 
+//module.exports = require(`./config/webpack.${confType}.js`);
+
 module.exports = require(`./config/webpack.${confType}.js`);
