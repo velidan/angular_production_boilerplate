582854eabd4498b5040a7bca3f94d181c7bf1cda
Sviatoslav Barbutsa <sbarbut@softserveinc.com>
2016-11-28 18:29:03 +0200

    - fixed test issue for router-outlet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
-describe('App', () => {
+describe('App test', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    TestBed.configureTestingModule({ declarations: [AppComponent],  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]});
   });
-  it ('should work', () => {
+  it ('App component should be an instance of itself', () => {
     let fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
   });
